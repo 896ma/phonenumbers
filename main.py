01e9e416d5d8db9c0576bca0b78590ca4e248f90
896ma <solarcrash3@gmail.com.com>
2023-08-23 22:32:43 +0300

    python number locator

---
+++ main.py
@@ -0,0 +1,11 @@
+import phonenumbers
+from test import  number       
+ 
+
+from phonenumbers  import geocoder
+ch_numbr =phonenumbers.parse(number ,"CH")
+print(geocoder.description_for_number(ch_numbr,"en"))
+
+from phonenumbers import carrier
+service_numbr =phonenumbers.parse(number,"RO")
+print(carrier.name_for_number(service_numbr ,"en"))
