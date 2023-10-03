88e91f5fe1df6bec34c3ca29839797dd0a54645b
G_oaT <100561547+896ma@users.noreply.github.com>
2023-10-03 15:59:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import phonenumbers
 from test import  number       
  
 
-from phonenumbers  import geocode
+from phonenumbers  import geocod
 ch_numbr =phonenumbers.parse(number ,"CH")
 print(geocoder.description_for_number(ch_numbr,"en"))
 
